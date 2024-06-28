be83330c3d62430cab402855b7846d3611c12f58
eduardop1p <el702985@gmail.com>
2024-06-28 16:18:19 -0300

    adicionando estados

--- src/components/forms/inputUF/index.tsx
+++ src/components/forms/inputUF/index.tsx
@@ -27,33 +27,33 @@ export default function InputUF({ label, name, register, errors }: Props) {
           defaultValue=""
         >
           <option value="">Selecionar</option>
-          <option value="Acre">Acre</option>
-          <option value="Alagoas">Alagoas</option>
-          <option value="Amapá">Amapá</option>
-          <option value="Amazonas">Amazonas</option>
-          <option value="Bahia">Bahia</option>
+          {/* <option value="Acre">Acre</option> */}
+          {/* <option value="Alagoas">Alagoas</option> */}
+          {/* <option value="Amapá">Amapá</option> */}
+          {/* <option value="Amazonas">Amazonas</option> */}
+          {/* <option value="Bahia">Bahia</option> */}
           <option value="Ceará">Ceará</option>
           <option value="Distrito Federal">Distrito Federal</option>
           <option value="Espírito Santo">Espírito Santo</option>
-          <option value="Goiás">Goiás</option>
-          <option value="Maranhão">Maranhão</option>
-          <option value="Mato Grosso">Mato Grosso</option>
-          <option value="Mato Grosso do Sul">Mato Grosso do Sul</option>
+          {/* <option value="Goiás">Goiás</option> */}
+          {/* <option value="Maranhão">Maranhão</option> */}
+          {/* <option value="Mato Grosso">Mato Grosso</option> */}
+          {/* <option value="Mato Grosso do Sul">Mato Grosso do Sul</option> */}
           <option value="Minas Gerais">Minas Gerais</option>
-          <option value="Pará">Pará</option>
-          <option value="Paraíba">Paraíba</option>
+          {/* <option value="Pará">Pará</option> */}
+          {/* <option value="Paraíba">Paraíba</option> */}
           <option value="Paraná">Paraná</option>
-          <option value="Pernambuco">Pernambuco</option>
+          {/* <option value="Pernambuco">Pernambuco</option> */}
           <option value="Piauí">Piauí</option>
-          <option value="Rio de Janeiro">Rio de Janeiro</option>
-          <option value="Rio Grande do Norte">Rio Grande do Norte</option>
+          {/* <option value="Rio de Janeiro">Rio de Janeiro</option> opções de cpf a fazer */}
+          {/* <option value="Rio Grande do Norte">Rio Grande do Norte</option> */}
           <option value="Rio Grande do Sul">Rio Grande do Sul</option>
-          <option value="Rondônia">Rondônia</option>
-          <option value="Roraima">Roraima</option>
+          {/* <option value="Rondônia">Rondônia</option> */}
+          {/* <option value="Roraima">Roraima</option> */}
           <option value="Santa Catarina">Santa Catarina</option>
           <option value="São Paulo">São Paulo</option>
-          <option value="Sergipe">Sergipe</option>
-          <option value="Tocantins">Tocantins</option>
+          {/* <option value="Sergipe">Sergipe</option> */}
+          {/* <option value="Tocantins">Tocantins</option> */}
         </select>
         {error && <ErrorMsg>{error.message}</ErrorMsg>}
       </div>
