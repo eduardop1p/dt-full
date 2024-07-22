92e94ce7499e6763472e6c10414023562af6a718
eduardop1p <el702985@gmail.com>
2024-07-22 08:18:27 -0300

    adicionando ano

--- src/components/totalDebts/index.tsx
+++ src/components/totalDebts/index.tsx
@@ -1,10 +1,12 @@
 import formatPrice from '@/services/formatPrice';
 
 export default function TotalDebts({ amount }: { amount: number }) {
+  const year = new Date().getFullYear();
+
   return (
     <div className="flex items-center w-full justify-between gap-8 bg-c0c0c0  px-2 py-[2px]">
       <h2 className="text-black font-bold text-[11px]">
-        Valor total - IPVA 2024
+        Valor total - IPVA {year}
       </h2>
       <span className="text-black font-bold text-[11px]">
         {formatPrice(amount)}
